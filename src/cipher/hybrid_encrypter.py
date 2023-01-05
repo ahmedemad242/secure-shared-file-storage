7e6c1a404ce2f765fbaf240c190d80cd70e46f68
Ahmed Emad <50369848+ahmedemad242@users.noreply.github.com>
2023-01-05 09:53:03 +0200

    feat: add hybrid encrypt-er

---
+++ src/cipher/hybrid_encrypter.py
@@ -0,0 +1,123 @@
+"""
+Hybrid encrypter class
+"""
+
+from typing import Tuple, List, Generator
+from Cryptodome.Random import get_random_bytes
+
+from .AES import AESCipher
+from .DES import DESCipher
+from .abstract_cipher import Cipher
+
+
+class HybridEncrypter:
+    """
+    Hybrid encrypter class
+    """
+
+    @staticmethod
+    def encrypt(raw: bytes) -> Tuple[bytes, bytes]:
+        """
+        Encrypt raw data using hybrid encryption
+
+        paramaters
+        ----------
+        raw: bytes
+            Raw data
+
+        returns
+        -------
+        Tuple[bytes, bytes]
+            Encrypted data and keys used for encyption (AES key (16 bytes) + DES key (8 bytes))
+        """
+
+        def roundRobinCipher(ciphers: List[Cipher]) -> Generator[Cipher, None, None]:
+            """
+            Round robin cipher
+            """
+
+            while True:
+                for cipher in ciphers:
+                    yield cipher
+
+        chunckSize = 16
+
+        keyAes = get_random_bytes(16)
+        keyDes = get_random_bytes(8)
+
+        aes = AESCipher(keyAes)
+        des = DESCipher(keyDes)
+
+        cipherGenetator = roundRobinCipher([aes, des, des])
+
+        if len(raw) % chunckSize != 0:
+            raw += b" " * (chunckSize - len(raw) % chunckSize)
+
+        currChunck = 0
+        encryptedData = b""
+
+        while True:
+            chunk = raw[currChunck : currChunck + chunckSize]
+            currChunck += chunckSize
+
+            if len(chunk) == 0:
+                break
+            if len(chunk) % 16 != chunckSize:
+                chunk += b" " * (chunckSize - len(chunk))
+
+            encryptedData += next(cipherGenetator).encrypt(chunk)
+
+        return (encryptedData, keyAes + keyDes)
+
+    @staticmethod
+    def decrypt(encryptedData: bytes, keys: bytes) -> bytes:
+        """
+        Decrypt encrypted data
+
+        paramaters
+        ----------
+        enc: bytes
+            Encrypted data
+        keys: bytes
+            Keys used for encryption (AES key (16 bytes) + DES key (8 bytes))
+
+        returns
+        -------
+        bytes
+            Decrypted data
+        """
+
+        def roundRobinCipher(ciphers: List[Cipher]) -> Generator[Cipher, None, None]:
+            """
+            Round robin cipher
+            """
+
+            while True:
+                for cipher in ciphers:
+                    yield cipher
+
+        chunckSize = 16
+
+        keyAes = keys[:16]
+        keyDes = keys[16:]
+
+        aes = AESCipher(keyAes)
+        des = DESCipher(keyDes)
+
+        cipherGenetator = roundRobinCipher([aes, des, des])
+
+        currChunck = 0
+        decryptedData = b""
+
+        while True:
+            chunk = encryptedData[currChunck : currChunck + chunckSize]
+            currChunck += chunckSize
+            if len(chunk) == 0:
+                break
+            if len(chunk) % 16 != chunckSize:
+                chunk += b" " * (chunckSize - len(chunk))
+
+            decryptedData += next(cipherGenetator).decrypt(chunk)
+            decryptedData = decryptedData.rstrip()
+
+        return decryptedData
